9b703577c55139262bfd87dc5cb0c146e261d69b
froisfernanda <127303960+froisfernanda@users.noreply.github.com>
2023-08-07 16:50:34 -0300

    Add files via upload

---
+++ JavaScript-main/fernanda/1-meuPrimeiroPrograma.js
@@ -0,0 +1,14 @@
+//Console.log("Meu Primeiro Programa: Trabalhando com varíaveis");
+console.log("Meu Primeiro Programa: Trabalhando com varíaveis");
+//JS é case sensitive
+
+const idade = 26;
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
---
+++ JavaScript-main/fernanda/2-trabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+//console.log("trabalhandoComVariaveis");
+console.log("trabalhandoComVariaveis");
+//JS é case senvitive
+
+const idade = 29;
+const nome = "Ricardo";
+
+//não fazer
+//mes = "janeiro";
