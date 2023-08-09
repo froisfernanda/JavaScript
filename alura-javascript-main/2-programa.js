e3ffa8c3c6bc6e79b31d09d8616de28527b17732
froisfernanda <127303960+froisfernanda@users.noreply.github.com>
2023-08-09 09:09:19 -0300

    Add files via upload

---
+++ alura-javascript-main/2-programa.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com Variáveis");
+// js é case sensitive
+
+const idade = 15;
+const nome = "Fernanda";
+
+console.log(idade, nome);
+
+// nao fazer
+//mes = "Janeiro"
---
+++ alura-javascript-main/5-programa.js
@@ -0,0 +1,23 @@
+console.log("Trabalhando com Atrb de Variáveis");
+
+let primeiroNome = "Fernanda"; // let = varia (pode ser modificado) 
+const sobrenome = "Frois";
+
+
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNomeNome} ${sobrenome}`);
+
+
+const nomeCompleto = primeiroNome + sobrenome;
+
+
+console.log(nome);
+
+
+let contador = 0;
+contador = contador + 1;
+
+
+let idade;
+idade = 16;
+idade = idade + 1;
